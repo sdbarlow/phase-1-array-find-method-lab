52a9ced6f253c2123cf69f45c60b37c17447b212
Seth Barlow <sethdbarlow@gmail.com>
2023-02-03 12:07:52 -0700

    all tests pass

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "L"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+  function superbowlWin(obj){
+    const result = obj.find(element => element.result ==="W")
+        if(result === undefined){
+            return undefined
+        }else{
+            return result.year;
+        }
+    
+  }
+
+
+  console.log(superbowlWin(record))
